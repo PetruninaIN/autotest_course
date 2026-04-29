0238d96f93f57554801d12732be7593e66ef23c6
in.petrunina <in.petrunina@tensor.ru>
2026-04-29 12:34:42 +0400

    Задание 2 решено

--- Homework6/task2.py
+++ Homework6/task2.py
@@ -7,8 +7,10 @@
 def global_function():
   msg = 1
   def local_function():
-  # Здесь нужно написать код
-   
+    nonlocal msg
+    msg = 2
+
+  local_function()
   return msg
 
 assert global_function() == 2, 'Значение переменной msg должно быть равно 2'
